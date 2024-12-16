052a01a46eeae4692986cc547e70de7e259493ab
uzairr <uzairr@yahoo.com>
2024-12-16 13:18:29 +0500

    add space

--- pages/tests.py
+++ pages/tests.py
@@ -2,10 +2,13 @@ from django.test import SimpleTestCase
 
 
 class SimpleTests(SimpleTestCase):
-	def test_home_page_status_code(self):
+	def test_home_page_status_code_succes(self):
+
 		response = self.client.get('/')
+		
 		self.assertEqual(response.status_code, 200)
 
-	def test_about_page_status_code(self):
+	def test_about_page_status_code_success(self):
+
 		response = self.client.get('/about/')
 		self.assertEqual(response.status_code, 200)
