680df89d4c71bb3ca0afae75fce2f808bec3ee05
uzairr <uzairr@yahoo.com>
2024-12-18 13:15:27 +0500

    invoking integration flow

--- pages/tests.py
+++ pages/tests.py
@@ -6,6 +6,6 @@ class SimpleTests(SimpleTestCase):
 		response = self.client.get('/')
 		self.assertEqual(response.status_code, 200)
 
-	def test_about_page_status_code(self):
+	def test_about_page_status_code_expt(self):
 		response = self.client.get('/about/')
 		self.assertEqual(response.status_code, 200)
